6a91dd592ae80362ce3fc9823fe54691b8abd462
queen-x-vee <valentinabuoro4@gmail.com>
2022-10-08 19:06:50 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from './components/Login/LoginPage';
+import SmoothNavHoverEffect from './components/Navbar/SmoothNavHoverEffect';
 
 function App() {
   return (
     <Routes>
       <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/navbar' element={<SmoothNavHoverEffect/>}/>
     </Routes>
     
   );
---
+++ src/components/Navbar/SmoothNavHoverEffect.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './SmoothNavHoverEffect.css'
+
+function SmoothNavHoverEffect() {
+  return (
+    
+        <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Product</li>
+        <li>Contact</li>
+    </ul>
+    
+    
+  )
+}
+
+export default SmoothNavHoverEffect
